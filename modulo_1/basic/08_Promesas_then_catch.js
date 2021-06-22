da84904aed18bd935bc3e51d6573fc27c844531c
EduardoET <eduardotaco42@gmail.com>
2021-06-22 12:22:38 -0500

    promesas

--- modulo_1/basic/08_Promesas_then_catch.js
+++ modulo_1/basic/08_Promesas_then_catch.js
@@ -13,10 +13,43 @@ function promesa(bandera){
         }
     })
 }
-promesa(false)
-    .then((resultado) => {
-        console.log("RESPUESTA POSITIVA",resultado)
+// promesa(false)
+//     .then((resultado) => {
+//         console.log("RESPUESTA POSITIVA",resultado)
+//     })
+//     .catch((error) => {
+//         console.log("ERROR", error)
+//     })
+
+
+var segundaPromesa = () => new Promise((resolve, reject) => {
+    if (bandera) {
+        setTimeout(() => {
+            resolve("PETICION RESUELTA");
+        }, 3000);
+    } else {
+        setTimeout(() => {   
+        reject("LA OPREACION FALLO")
+        }, 2000);
+    }
+})
+
+segundaPromesa(true)
+    .then ((respuesta) => {
+        console.log(respuesta)
     })
     .catch((error) => {
-        console.log("ERROR", error)
+        console.log(error)
+    })
+
+promesa(true) //peticion de clientes
+    .then((respuesta) => { //respuesta de lista de cliente
+        console.log(respuesta)
+        return segundaPromesa(true) //compras del cliente
+    })
+    .then((respuesta) => { //respuesta de la segunda promesa
+        console.log(respuesta)
+    })
+    .catch((error) => {
+        console.log(error)
     })
