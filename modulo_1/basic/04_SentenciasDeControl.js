144a4079a7191e0cae19c4164be36f17c034e857
EduardoET <eduardotaco42@gmail.com>
2021-06-22 01:28:51 -0500

    commit primero

--- modulo_1/basic/04_SentenciasDeControl.js
+++ modulo_1/basic/04_SentenciasDeControl.js
@@ -4,9 +4,24 @@ var bandera = true;
     console.log("es verdadero")
 
 }*/
-if (bandera) {
+
+/*if (bandera) {
     console.log("es verdadero")
+} if (bandera){
+
+} if (bandera){
+
 } else {
 
     console.log("es falso")
 }
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+
