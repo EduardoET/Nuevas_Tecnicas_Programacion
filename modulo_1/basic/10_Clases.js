09dce16cfad8a938b6851207484ddad61a27862e
EduardoET <eduardotaco42@gmail.com>
2021-06-23 19:58:03 -0500

    deber

--- modulo_1/basic/10_Clases.js
+++ modulo_1/basic/10_Clases.js
@@ -26,3 +26,44 @@ class Rectangulo2 {
 
 var rectangulo2 = new Rectangulo2(6, 3)
 console.log(rectangulo.calcularArea())
+
+/* 
+    DEBER 
+*/
+
+class  Punto {
+    constructor ( x , y ) {
+        esto.x  =  x ;
+        esto.y  =  y ;
+    }
+    estática  Distancia(a,b) {
+const  dx  = a.x - b.x 
+const  dy  = a.y - b.y 
+ devuelve Math.sqrt( dx  *  dx +  dy  *  dy ) 
+    }
+   
+}
+const  p  =  nuevo Punto( 15 , 7 ) 
+const  punto  =  new Punto ( 6 , 8 ) 
+
+consola . log ( punto.Distancia ( p , punto ) ) 
+// CLASE PERSONA Y ESTUDIANTE
+class  Personaa {
+    constructor ( nombre ) {
+        esto . nombre  =  nombre ;
+        esto . apellido  =  apellido ;
+    }
+
+}
+consola . registro ( Personaa ) ;
+  estudiante de clase extiende  Personaa {
+     constructor ( nombre , apellido , edad ) {
+         super ( nombre ) ;
+        super ( apellido ) ;
+        esto . edad ;
+
+     }
+ }
+ consola . registro ( Personaa ) ;
+ var  persona  =  nueva  Persona( "EDUARDO" ) ;
+ var  estudiante  =  new  estudiantee ( "EDUARDO" , "TACO" , 26 ) ;
