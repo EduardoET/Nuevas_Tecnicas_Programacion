6111589aa924e7f638a64740e43bee8837b7a4c7
EduardoET <eduardotaco42@gmail.com>
2021-06-24 12:03:47 -0500

    clase 24-06

--- modulo_1/programa/program/01_Project/index.js
+++ modulo_1/programa/program/01_Project/index.js
@@ -4,7 +4,33 @@ const express = require('express')
 const app = express ()
 //endpoint
 app.get('/', (req, res)=>{
-    res.send("HOLA MUNDO CON EXPRESS JS")
+    res.send("HOLA MUNDO CON EXPRESSJS")
+})
+
+app.get("/saludo", (req, res) => {
+    //req.query
+
+    //console.log(req, query )
+    const { query: {nombre, apellido} 
+     } = req;
+
+    // const nombre = req.query.nombre
+    // const apellido = req.query.apellido
+
+    res.json({
+        saludo : `HOLA SOY ${nombre} ${apellido}`,
+    })
+})
+
+app.get('/saludo/:nombre', (req, res) => {
+        const {
+            params: {nombre},
+        }= req 
+
+        //const nombre = req.params.nombre
+        res.json({
+            nombre: nombre
+        })
 })
 
 app.listen(3000, ()=>{
---
+++ modulo_1/programa/program/01_Project/routers/index.js
@@ -0,0 +1,9 @@
+//traemos el paquete 
+const { Router } = require('express')
+
+//inicializamos router
+const router = Router()
+
+//creacion de endpoint
+
+module.exports.RouterIndex = router 
