cbe9914561eb0d77b757d677f01268c0e062bf5f
EduardoET <eduardotaco42@gmail.com>
2021-06-25 11:37:12 -0500

    clase 25-06

--- modulo_1/programa/program/01_Project/index.js
+++ modulo_1/programa/program/01_Project/index.js
@@ -1,37 +1,17 @@
+const { Router } = require('express')
+
+
 const express = require('express')
 
 //genera una aplicacion express 
 const app = express ()
+
 //endpoint
-app.get('/', (req, res)=>{
-    res.send("HOLA MUNDO CON EXPRESSJS")
-})
+const {RoterIndex, RouterIndex} = require('./routers/index')
 
-app.get("/saludo", (req, res) => {
-    //req.query
+app.use("/", RouterIndex)
 
-    //console.log(req, query )
-    const { query: {nombre, apellido} 
-     } = req;
 
-    // const nombre = req.query.nombre
-    // const apellido = req.query.apellido
-
-    res.json({
-        saludo : `HOLA SOY ${nombre} ${apellido}`,
-    })
-})
-
-app.get('/saludo/:nombre', (req, res) => {
-        const {
-            params: {nombre},
-        }= req 
-
-        //const nombre = req.params.nombre
-        res.json({
-            nombre: nombre
-        })
-})
 
 app.listen(3000, ()=>{
     console.log("servidor escuchando en http://localhost:3000")
--- modulo_1/programa/program/01_Project/routers/index.js
+++ modulo_1/programa/program/01_Project/routers/index.js
@@ -6,4 +6,33 @@ const router = Router()
 
 //creacion de endpoint
 
+.get('/', (req, res)=>{
+    res.send("HOLA MUNDO CON EXPRESSJS")
+})
+
+
+.get("/saludo", (req, res) => {
+    //req.query
+
+    //console.log(req, query )
+    const { query: {nombre, apellido} 
+     } = req;
+
+    // const nombre = req.query.nombre
+    // const apellido = req.query.apellido
+
+    res.json({
+        saludo : `HOLA SOY ${nombre} ${apellido}`,
+    })
+})
+.get('/saludo/:nombre', (req, res) => {
+    const{ params} = r
+    //const nombre = req.params.nombre
+    res.json({
+        nombre: params.nombre
+    })
+})
+
+
+//exportamos las rutas
 module.exports.RouterIndex = router 
